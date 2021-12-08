86e4f20b4e49b1c78da6f84ad6e6730390d4ef8d
efraimrocha <67542881+efraimrocha@users.noreply.github.com>
2021-12-07 21:41:49 -0300

    Create alunoView.py

---
+++ view/alunoView.py
@@ -0,0 +1,40 @@
+from view.Tkfull import Janela
+from time import sleep, time
+from controller.alunoController import AlunoController
+
+class AlunoView:
+
+        vetor=[[' ','CADASTRO DE ALUNO'],
+                ['Matrícula',input],
+                ['Nome',input],
+                ['E-mail',input],
+                ['Gênero',('Feminino','Masculino')],
+                ['Senha',complex],
+                [{'*Sair':'assets/img/sair2.png'},' ',{'*Cadastrar':'assets/img/save.png'}],
+                ]
+
+        def __init__(self):
+                self.jan = Janela()
+                self.jan.gerar(self.vetor)
+                self.jan.setEvento(15,self.Cadastrar)
+                self.jan.setEvento(13,self.Sair)
+                self.jan.start()
+
+
+        def Sair(self):
+                msn = "O programa será emcerrado"
+                self.jan.mensagem(msn)
+                sleep(1)
+                quit()
+
+        def Cadastrar(self):
+                self.Salvar()
+                dados = AlunoController()
+                msn = dados.pegarDados(self.jan)
+                self.jan.mensagem(msn)
+        
+        def Salvar(self):
+                save=AlunoController()
+                save.gravarDados()
+
+AlunoView()
