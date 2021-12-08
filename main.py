184666f17f962ec4fceee235ee6ae12f2b7c2973
efraimrocha <67542881+efraimrocha@users.noreply.github.com>
2021-12-07 21:44:22 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+from view.alunoView import AlunoView
+
+
+# def pegarCaminho(self):
+#     try:
+#         os.chdir(os.path.dirname(__file__))
+#         rootdir=str(os.getcwd())
+#         return rootdir
+#     except:
+#         print("")
+    
+# pegarCaminho()
+AlunoView()
